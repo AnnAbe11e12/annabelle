a34c819ab5170280ccd16692de4613f2048f0604
Annabelle <lilacpaw2018@gmail.com>
2024-01-03 12:24:30 -1000

    finished introduction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-
+import mainStyles from './main-styles.css'
 import createWordSlider from './scrolling-text.js'
-
 
 
 let scrollTextContainer = document.querySelector('.scroll-text-container');
@@ -8,9 +7,5 @@ let scrollTextList = document.querySelectorAll('.scroll-test')
 
 
 window.addEventListener('load',()=>{
-    console.log('load event fires')
-    console.log('container '+scrollTextContainer)
-    console.log('list '+scrollTextList)
-    console.log(createWordSlider)
     createWordSlider(scrollTextContainer, scrollTextList)
 })
